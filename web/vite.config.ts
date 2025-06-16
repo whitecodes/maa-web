4f431a374f39f71e30acecdb98a66e4fd29d400b
whitecodes <whitemailg@gmail.com>
2025-06-17 00:42:28 +0800

    remove test setting

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -13,9 +13,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // test: {
-  //   globals: true,
-  //   environment: 'jsdom',  // 使用 jsdom 作为测试环境模拟浏览器环境
-  //   setupFiles: 'tests/setup.ts'  // 指定测试的设置文件
-  // }
 })
